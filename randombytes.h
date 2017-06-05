02282131fe4e33ce665e363633fe3bfe980a9aea
Daan Sprenkels <hello@dsprenkels.com>
2017-06-05 16:59:09 +0200

    Add randombytes submodule

--- randombytes.h
+++ randombytes.h
@@ -1,14 +1,1 @@
-#ifndef sss_RANDOMBYTES_H
-#define sss_RANDOMBYTES_H
-
-#include <sys/syscall.h>
-#include <unistd.h>
-
-
-/*
- * Write `n` bytes of high quality random bytes to `buf`
- */
-void randombytes(void *buf, size_t n);
-
-
-#endif /* sss_RANDOMBYTES_H */
+randombytes/randombytes.h
