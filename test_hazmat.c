8be93faddcd18ec8e43da0bd8139824e65425f86
Daan Sprenkels <hello@dsprenkels.com>
2017-08-07 23:49:41 +0700

    tests: Explicitly cast to const types

--- test_hazmat.c
+++ test_hazmat.c
@@ -14,19 +14,19 @@ static void test_key_shares()
 	}
 
 	sss_create_keyshares(key_shares, key, 1, 1);
-	sss_combine_keyshares(restored, key_shares, 1);
+	sss_combine_keyshares(restored, (const sss_Keyshare*) key_shares, 1);
 	assert(memcmp(key, restored, 32) == 0);
 
 	sss_create_keyshares(key_shares, key, 3, 2);
-	sss_combine_keyshares(restored, &key_shares[1], 2);
+	sss_combine_keyshares(restored, (const sss_Keyshare*) key_shares[1], 2);
 	assert(memcmp(key, restored, 32) == 0);
 
 	sss_create_keyshares(key_shares, key, 255, 127);
-	sss_combine_keyshares(restored, &key_shares[128], 127);
+	sss_combine_keyshares(restored, (const sss_Keyshare*) key_shares[128], 127);
 	assert(memcmp(key, restored, 32) == 0);
 
 	sss_create_keyshares(key_shares, key, 255, 255);
-	sss_combine_keyshares(restored, key_shares, 255);
+	sss_combine_keyshares(restored, (const sss_Keyshare*) key_shares, 255);
 	assert(memcmp(key, restored, 32) == 0);
 }
 
--- test_sss.c
+++ test_sss.c
@@ -10,24 +10,24 @@ int main()
 
 	/* Normal operation */
 	sss_create_shares(shares, data, 1, 1);
-	tmp = sss_combine_shares(restored, shares, 1);
+	tmp = sss_combine_shares(restored, (const sss_Share*) shares, 1);
 	assert(tmp == 0);
 	assert(memcmp(restored, data, sss_MLEN) == 0);
 
 	/* A lot of shares */
 	sss_create_shares(shares, data, 255, 255);
-	tmp = sss_combine_shares(restored, shares, 255);
+	tmp = sss_combine_shares(restored, (const sss_Share*) shares, 255);
 	assert(tmp == 0);
 	assert(memcmp(restored, data, sss_MLEN) == 0);
 
 	/* Not enough shares to restore secret */
 	sss_create_shares(shares, data, 100, 100);
-	tmp = sss_combine_shares(restored, shares, 99);
+	tmp = sss_combine_shares(restored, (const sss_Share*) shares, 99);
 	assert(tmp == -1);
 
 	/* Too many secrets should also restore the secret */
 	sss_create_shares(shares, data, 200, 100);
-	tmp = sss_combine_shares(restored, shares, 200);
+	tmp = sss_combine_shares(restored, (const sss_Share*) shares, 200);
 	assert(tmp == 0);
 	assert(memcmp(restored, data, sss_MLEN) == 0);
 
